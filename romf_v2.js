6a77c2bc7abf28fc92ce8c703be7f354e3c448b5
realmofmetal <123463146+realmofmetal@users.noreply.github.com>
2023-02-15 01:59:29 +0000

    Update and rename romf6.js to romf_v2.js

--- romf_v2.js
+++ romf_v2.js
@@ -43,10 +43,10 @@ const link = image.parentNode.href;
 const newElement = `<div class="imgbelow" style="font-size:12px"><table class="postimgaddons" style="width:100%"><tbody><tr><td><span style="float:left;line-height: 1.8em;font-size:12px;">Rate this release:</span><div class="realm-rating-true"/></td><td style="float:right"><a href="${link}" target="_blank" rel="nofollow"><span class="genre-filters"><i class="fa fa-arrow-down" aria-hidden="true"></i> MP3 DOWNLOAD</span></a> <a href="#comments"><span class="genre-filters"><i class="fa fa-times" aria-hidden="true"></i> DEAD LINK</span></a> <span style="color:red;font-size:8px">NEW</span></td></tr></tbody></table><div class="clear"/></div>`;
 separator.insertAdjacentHTML('afterend', newElement);}
 
-// Replace download links for uppercase domain names
+// Replace download links for uppercase domain names and add network links on olders posts
 window.onload = function() {
-const links = document.querySelectorAll('#tab-0-2 .inner a');
-links.forEach(function(link) {
+const links2 = document.querySelectorAll('#tab-0-2 .inner a');
+links2.forEach(function(link) {
   const urlanony = new URL(link.href.replace("http://anonymto.com/#", ""));
   let domain = urlanony.hostname;
   const domainParts = domain.split(".");
@@ -54,7 +54,6 @@ links.forEach(function(link) {
   link.innerHTML = domain + link.innerHTML;
 });
 
-// Add network links on older posts
 if (!window.location.href.includes("/p/") && 
     !window.location.href.includes("/2023/") && 
     window.location.href !=="https://wow.realmofmetal.org/"){var _1bit=new _1bitspace({public_key:"hAvVrB9-",only:["mega.nz","mediafire.com","yadi.sk","zippyshare.com","rgho.st","workupload.com","dl.realmofmetal.org","dwld.realmofmetal.org","cloud.mail.ru"]})}
