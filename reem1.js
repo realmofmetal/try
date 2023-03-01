d0efa0d59b55bd65c65490a1380e797942c45129
realmofmetal <123463146+realmofmetal@users.noreply.github.com>
2023-03-01 15:51:22 +0000

    Update and rename romok6.js to reem1.js

--- reem1.js
+++ reem1.js
@@ -26,6 +26,7 @@ $.each($("a[name]"), function(i, e) {
 // Run before all other link scripts
 if (!window.location.href.includes("/p/") && 
     !window.location.href.includes("/2023/02/") && 
+    !window.location.href.includes("/2023/03/") && 
     window.location.href !=="https://wow.realmofmetal.org/"){var _1bit=new _1bitspace({public_key:"hAvVrB9-",only:["mega.nz","mediafire.com","yadi.sk","zippyshare.com","rgho.st","workupload.com","dl.realmofmetal.org","dwld.realmofmetal.org","cloud.mail.ru"]})}
 
 // Find all external links and add attributes
@@ -45,7 +46,7 @@ if (!window.location.href.includes("/p/") &&
 const separator = document.querySelector('.separator');
 const image = separator.querySelector('img');
 const link = image.parentNode.href;
-const newElement = `<div class="imgbelow" style="font-size:12px"><table class="postimgaddons" style="width:100%"><tbody><tr><td><span style="float:left;line-height: 1.8em;font-size:12px;">Rate this release:</span><div class="realm-rating-true"/></td><td style="float:right"><a href="${link}" target="_blank" rel="nofollow" title="MP3 DOWNLOAD"><span class="genre-filters"><i class="fa fa-cloud-download" aria-hidden="true"></i> MP3</span></a> <a href="#comments" title="ASK NEW LINK"><span class="genre-filters"><i class="fa fa-chain-broken" aria-hidden="true"></i> BROKEN</span></a> <a href="/p/report.html" title="REPORT DMCA"><span class="genre-filters"><i class="fa fa-flag" aria-hidden="true"></i> REPORT</span></a></td></tr></tbody></table><div class="clear"/></div>`;
+const newElement = `<div class="imgbelow" style="font-size:12px"><table class="postimgaddons" style="width:100%"><tbody><tr><td><span style="float:left;line-height: 1.8em;font-size:12px;">Rate<div class="ratethis"> this release</div>:</span><div class="realm-rating-true"/></td><td style="float:right"><a href="${link}" target="_blank" rel="nofollow" title="MP3 DOWNLOAD"><span class="genre-filters"><i class="fa fa-cloud-download" aria-hidden="true"></i> MP3</span></a> <a href="#tab-0-3" title="ASK NEW LINK"><span class="genre-filters"><i class="fa fa-chain-broken" aria-hidden="true"></i> BROKEN</span></a> <a href="/p/report.html" title="REPORT DMCA"><span class="genre-filters"><i class="fa fa-flag" aria-hidden="true"></i> REPORT</span></a></td></tr></tbody></table><div class="clear"/></div>`;
 separator.insertAdjacentHTML('afterend', newElement);};
 
 
